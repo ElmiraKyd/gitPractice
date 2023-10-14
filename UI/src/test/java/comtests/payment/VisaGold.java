2e39880ed446dcc5af3b37ce019e569d8713598a
ElmiraKyd <147648145+ElmiraKyd@users.noreply.github.com>
2023-10-15 00:15:09 +0600

    Update VisaGold.java

--- UI/src/test/java/comtests/payment/VisaGold.java
+++ UI/src/test/java/comtests/payment/VisaGold.java
@@ -2,6 +2,6 @@ package comtests.payment;
 
 public class VisaGold {
     public static void main(String[] args) {
-        System.out.println("Visa Gold");
+        System.out.println("Visa Gold123");
     }
 }
